fb80a22bdca8700b707024b2837921b69228b452
patatos12321 <31021380+patatos12321@users.noreply.github.com>
2025-04-09 12:29:21 -0400

    cards into jsons

---
+++ WebApp/FamilyConquest.Common/Repositories/CardRepository.cs
@@ -0,0 +1,27 @@
+﻿using FamilyConquest.Common.Models.Cards;
+using FamilyConquest.Common.Models.Cards.Shared;
+using LiteDB;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace FamilyConquest.Common.Repositories
+{
+    public class CardRepository()
+    {
+        public CardList GetStarterDeck()
+        {
+
+        }
+
+        public CardList LoadAllUniqueCards() {
+            var potentialCards = Directory.GetFiles("", "*.json", new EnumerationOptions() { RecurseSubdirectories = true });
+            foreach (var potentialCard in potentialCards)
+            {
+                if (Enum.TryParse<CardIds>)
+                {
+
+                }
+            }
+        }
+    }
+}
